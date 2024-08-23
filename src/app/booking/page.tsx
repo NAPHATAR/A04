4d0bd550657544852652d8e19a1533f9bd697527
HEKYPTO <tsunyanapat@gmail.com>
2024-08-24 02:14:36 +0700

    FIX TESTS

--- src/app/booking/page.tsx
+++ src/app/booking/page.tsx
@@ -1,7 +1,13 @@
 export default function Booking() {
     return (
       <div>
-        <h1 className="text-3xl font-bold mb-4">Vaccine Booking</h1>
+        <div className="container mx-auto">
+            <div className="bg-gray-100 rounded-md shadow-lg pb-8 w-full">
+                <h1 className="text-4xl font-extrabold text-center text-gray-900 pt-8 mb-10">
+                    Vaccine Booking
+                </h1>
+            </div>
+        </div>
       </div>
     )
   }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,29 @@
+import Banner from '@/components/Banner'
 import Card from '../components/Card'
 
 export default function Home() {
   return (
-    <div className="flex justify-center py-8">
-      <div className="bg-gray-100 rounded-md shadow-lg p-8 max-w-7xl mx-auto">
-        <h1 className="text-4xl font-extrabold text-center text-gray-900 mb-10">
-          Participating Hospitals
-        </h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-          <Card 
-            hospitalName="Chulalongkorn Hospital" 
-            imgSrc="/img/chula.jpg"
-          />
-          <Card 
-            hospitalName="Rajavithi Hospital" 
-            imgSrc="/img/rajavithi.jpg"
-          />
-          <Card 
-            hospitalName="Thammasat University Hospital" 
-            imgSrc="/img/thammasat.jpg"
-          />
+    <div>
+      <Banner />
+      <div className="container mx-auto py-8">
+        <div className="bg-gray-100 rounded-md shadow-lg p-8 w-full">
+          <h1 className="text-4xl font-extrabold text-center text-gray-900 mb-10">
+            Participating Hospitals
+          </h1>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+            <Card 
+              hospitalName="Chulalongkorn Hospital" 
+              imgSrc="/img/chula.jpg"
+            />
+            <Card 
+              hospitalName="Rajavithi Hospital" 
+              imgSrc="/img/rajavithi.jpg"
+            />
+            <Card 
+              hospitalName="Thammasat University Hospital" 
+              imgSrc="/img/thammasat.jpg"
+            />
+          </div>
         </div>
       </div>
     </div>
